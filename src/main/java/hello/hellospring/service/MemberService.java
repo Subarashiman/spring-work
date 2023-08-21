8b626da8dc6e5a75c538197db1b688783975c826
SeungHwan Lim <tim02366@naver.com>
2023-08-22 00:24:15 +0900

    Day 3 study

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -23,8 +25,8 @@ public class MemberService {
 
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원 x
-        validateDuplicateMember(member); // 중복 회원 검증
 
+        validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
     }
